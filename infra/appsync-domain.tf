325ed08406737aa0bdd7fb859f726227f2140a60
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2022-12-01 17:14:06 +0100

    Update custom domain to be optional

--- infra/appsync-domain.tf
+++ infra/appsync-domain.tf
@@ -1,9 +1,11 @@
 resource "aws_appsync_domain_name" "main" {
+  count = var.certificate_arn != null && var.api_domain_name!=null?1:0
   domain_name     = var.api_domain_name
   certificate_arn = var.certificate_arn
 }
 
 resource "aws_appsync_domain_name_api_association" "main" {
+  count = var.certificate_arn != null && var.api_domain_name!=null?1:0
   api_id      = aws_appsync_graphql_api.main.id
-  domain_name = aws_appsync_domain_name.main.domain_name
+  domain_name = aws_appsync_domain_name.main[0].domain_name
 }
